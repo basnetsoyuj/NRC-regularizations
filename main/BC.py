7edb5a4ff5e444261a246818d412ac1deef05786
Konokiii <zd662@nyu.edu>
2024-05-05 01:28:07 +0400

    explore NC2

--- main/BC.py
+++ main/BC.py
@@ -34,12 +34,14 @@ class TrainConfig:
     batch_size: int = 256  # Batch size for all networks
     num_eval_batch: int = 100  # Do NC evaluation over a subset of the whole dataset
     data_ratio: float = 1.0  # Reduce Swimmer data, too many of them
+    normalize: str = 'none'
 
-    arch: str = '256-256'  # Actor architecture
+    arch: str = '256-R-256-R|False'  # Actor architecture
     reg_coff_H: float = 1e-5  # If it is -1, then the model is not UFM.
     reg_coff_W: float = 5e-2
     lr: float = 3e-4
 
+    mode: str = 'null'
     data_folder: str = '/NC_regression/dataset/mujoco'
 
     # Wandb logging
@@ -77,29 +79,51 @@ def compute_metrics(metrics, device):
     yhat = metrics['outputs']  # (B,2)
     W = metrics['weights']  # (2,256)
     H = metrics['embeddings']  # (B,256)
-    print("Y", y.shape)
-    print("Y_hat", yhat.shape)
-    print("W", W.shape)
-    print("W0", W[0].shape)
-    print("H", H.shape)
 
     result['prediction_error'] = F.mse_loss(y, yhat).item()
     result['cosSIM_y_yhat'] = cosine_similarity_gpu(y, yhat).mean().item()
 
-    # W analysis
-    result['W1_norm_sq'] = torch.square(W[0].norm()).item()
-    result['W2_norm_sq'] = torch.square(W[1].norm()).item()
+    # WWT
+    WWT = W @ W.T
+    result['W11'] = WWT[0, 0].item()
+    result['W22'] = WWT[1, 1].item()
+    result['W12'] = WWT[0, 1].item()
     result['cosSIM_W12'] = F.cosine_similarity(W[0], W[1], dim=0).item()
+    result['WWT_norm'] = torch.norm(WWT).item()
+    del WWT
 
-    # H with PCA
-    H_norm_np = H.cpu().numpy()
+    # NC1
+    H_np = H.cpu().numpy()
     pca_for_H = PCA(n_components=2)
-    H_pca = pca_for_H.fit_transform(H_norm_np)
+    H_pca = pca_for_H.fit_transform(H_np)
     H_reconstruct = pca_for_H.inverse_transform(H_pca)
-    result['PCA_reconstruction_error'] = np.square(H_norm_np - H_reconstruct).sum(axis=1).mean().item()
-    del H_norm_np
-    del pca_for_H
+    result['PCA_reconstruction_error'] = np.square(H_np - H_reconstruct).sum(axis=1).mean().item()
     del H_reconstruct
+
+    try:
+        inverse_mat = np.linalg.inv(H_pca.T @ H_pca)
+    except Exception as e:
+        print(e)
+        result['NC1'] = -1
+    else:
+        H_proj_PCA = H_pca @ inverse_mat @ H_pca.T @ H_np
+        result['NC1'] = np.linalg.norm(H_np - H_proj_PCA, ord='fro').item()
+    del H_np
+    del H_proj_PCA
+    del H_pca
+    del pca_for_H
+
+    # NC3
+    try:
+        inverse_mat = torch.inverse(W.T @ W)
+    except Exception as e:
+        print(e)
+        result['NC3'] = -1
+    else:
+        H_proj_W = W @ inverse_mat @ W.T @ H
+        result['NC3'] = torch.norm(H - H_proj_W).item()
+
+    del H_proj_W
 
     # Cosine similarity of Y and H post PCA
     H_pca_tensor = torch.tensor(H_pca, dtype=torch.float32).to(device)
@@ -156,7 +180,8 @@ class MujocoBuffer(Dataset):
             data_folder: str,
             env: str,
             split: str,
-            data_ratio,
+            data_ratio: float,
+            normalize: str,
             device: str = "cpu",
     ):
         self.size = 0
@@ -164,7 +189,7 @@ class MujocoBuffer(Dataset):
         self.action_dim = 0
 
         self.states, self.actions = None, None
-        self._load_dataset(data_folder, env, split, data_ratio)
+        self._load_dataset(data_folder, env, split, data_ratio, normalize)
 
         self.device = device
 
@@ -172,13 +197,15 @@ class MujocoBuffer(Dataset):
         return torch.tensor(data, dtype=torch.float32, device=self.device)
 
     # Loads data in d4rl format, i.e. from Dict[str, np.array].
-    def _load_dataset(self, data_folder: str, env: str, split: str, data_ratio: float):
+    def _load_dataset(self, data_folder: str, env: str, split: str, data_ratio: float, normalize: str):
         file_name = '%s_%s.pkl' % (env, split)
         file_path = os.path.join(data_folder, file_name)
         try:
             with open(file_path, 'rb') as file:
                 dataset = pickle.load(file)
                 self.size = int(dataset['observations'].shape[0] * data_ratio)
+                if env == 'swimmer':  # Swimmer has too many data, so by default we use 10% of them.
+                    self.size = int(self.size * 0.1)
                 self.states = dataset['observations'][:self.size, :]
                 self.actions = dataset['actions'][:self.size, :]
             print('Successfully load dataset from: ', file_path)
@@ -187,6 +214,18 @@ class MujocoBuffer(Dataset):
 
         self.state_dim = self.states.shape[1]
         self.action_dim = self.actions.shape[1]
+
+        if normalize == 'none':
+            pass
+        elif normalize == 'standard':
+            mean = self.actions.mean(0)
+            std = np.maximum(self.actions.std(0), 1e-4)
+            self.actions = (self.actions - mean) / std
+        elif normalize == 'normal':
+            min_action = self.actions.min(axis=0)
+            max_action = self.actions.max(axis=0)
+            self.actions = (self.actions - min_action) / (max_action - min_action)
+
         print(f"Dataset size: {self.size}; State Dim: {self.state_dim}; Action_Dim: {self.action_dim}.")
 
     def get_state_dim(self):
@@ -287,18 +326,27 @@ def RL_eval(
 
 
 class Actor(nn.Module):
-    def __init__(self, state_dim: int, action_dim: int, max_action: float = 1.0, arch: str = '256-256'):
+    def __init__(self, state_dim: int, action_dim: int, max_action: float = 1.0, arch: str = '256-R-256-R'):
         super(Actor, self).__init__()
+
+        arch, use_bias = arch.split('|')
+        arch = arch[0].split('-')
+        use_bias = bool(use_bias[0])
 
         in_dim = state_dim
         module_list = []
-        for i, hidden_size in enumerate(arch.split('-')):
-            hidden_size = int(hidden_size)
-            module_list.append(nn.Linear(in_dim, hidden_size))
-            module_list.append(nn.ReLU())
-            in_dim = hidden_size
+        for i, layer in enumerate(arch):
+            if layer == 'R':
+                module_list.append(nn.ReLU())
+            elif layer == 'T':
+                module_list.append(nn.Tanh())
+            else:
+                out_dim = int(layer)
+                module_list.append(nn.Linear(in_dim, out_dim))
+                in_dim = out_dim
+
         self.feature_map = nn.Sequential(*module_list)
-        self.W = nn.Linear(in_dim, action_dim, bias=False)
+        self.W = nn.Linear(in_dim, action_dim, bias=use_bias)
 
         self.max_action = max_action
 
@@ -364,7 +412,7 @@ class BC:
             reg_W_loss = 0.5 * self.reg_coff_W * torch.norm(self.actor.W.weight) ** 2
             train_loss = mse_loss + reg_H_loss + reg_W_loss
 
-        log_dict["train_loss"] = train_loss.item()
+        log_dict["train_mse_loss"] = mse_loss.item()
         # Optimize the actor
         self.actor_optimizer.zero_grad()
         train_loss.backward()
@@ -420,6 +468,7 @@ def run_BC(config: TrainConfig):
         env=config.env,
         split='train',
         data_ratio=config.data_ratio,
+        normalize=config.normalize,
         device=config.device
     )
     val_dataset = MujocoBuffer(
@@ -427,6 +476,7 @@ def run_BC(config: TrainConfig):
         env=config.env,
         split='test',
         data_ratio=config.data_ratio,
+        normalize=config.normalize,
         device=config.device
     )
 
@@ -469,6 +519,12 @@ def run_BC(config: TrainConfig):
     # TODO: fix and optimize wandb log.
     train_theory_stats = train_dataset.get_theory_stats()
     val_theory_states = val_dataset.get_theory_stats()
+    if config.reg_coff_H != -1:
+        for d in [train_theory_stats, val_theory_states]:
+            d['A11'] = (config.reg_coff_H / config.reg_coff_W) ** 0.5 * d['sigma11'] - config.reg_coff_H
+            d['A22'] = (config.reg_coff_H / config.reg_coff_W) ** 0.5 * d['sigma22'] - config.reg_coff_H
+            d['A12'] = (config.reg_coff_H / config.reg_coff_W) ** 0.5 * d['sigma12']
+
     train_log = trainer.NC_eval(train_loader)
     val_log = trainer.NC_eval(val_loader)
     wandb.log({'train': train_log,
@@ -482,7 +538,7 @@ def run_BC(config: TrainConfig):
         count = 0
         for batch in tqdm(train_loader, desc=f"Epoch {epoch}/{config.max_epochs} Training"):
             log_dict = trainer.train(batch)
-            epoch_train_loss += log_dict['train_loss']
+            epoch_train_loss += log_dict['train_mse_loss']
             count += 1
             # wandb.log(trainer.train(batch), step=trainer.total_it)
         epoch_train_loss /= count
@@ -495,3 +551,28 @@ def run_BC(config: TrainConfig):
                        'trainConstant': train_theory_stats,
                        'valConstant': val_theory_states
                        })
+
+    # NC2
+    W = actor.W.weight.detach().clone().cpu().numpy()
+    WWT = W @ W.T
+    WWT_normalized = WWT / np.linalg.norm(WWT)
+    min_eigval = train_theory_stats['min_eigval']
+    Sigma_sqrt = np.array([train_theory_stats[k] for k in ['sigma11', 'sigma12', 'sigma21', 'sigma22']]).reshape(2, 2)
+    c_to_plot = np.linspace(0, min_eigval, num=1000)
+    NC2_to_plot = []
+    for c in c_to_plot:
+        c_sqrt = c ** 0.5
+        A = Sigma_sqrt - c_sqrt * np.eye(2)
+        A_normalized = A / np.linalg.norm(A)
+        NC2_to_plot.append(np.linalg.norm(WWT_normalized - A_normalized).item())
+
+    data = [[x, y] for (x, y) in zip(c_to_plot, NC2_to_plot)]
+    table = wandb.Table(data=data, columns=["c", "NC2"])
+    wandb.log(
+        {
+            "NC2(c)": wandb.plot.line(
+                table, "c", "NC2", title="NC2 as a Function of c"
+            )
+        }
+    )
+
---
+++ main/exp/initiative/NC2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --verbose
+#SBATCH --time=8:00:00
+#SBATCH --nodes=1
+#SBATCH --exclude=gm[002-020],gm[022-023],gm024
+#SBATCH --mem=16GB
+#SBATCH --mail-type=ALL # select which email types will be sent
+#SBATCH --mail-user=zd662@nyu.edu # NOTE: put your netid here if you want emails
+
+#SBATCH --array=0-287 # here the number depends on number of tasks in the array, e.g. 0-11 will create 12 tasks
+#SBATCH --output=../logs/%A_%a.out # %A is SLURM_ARRAY_JOB_ID, %a is SLURM_ARRAY_TASK_ID,
+#SBATCH --error=../logs/%A_%a.err # MAKE SURE WHEN YOU RUN THIS, ../train_logs IS A VALID PATH
+
+# #####################################################
+# #SBATCH --gres=gpu:1 # uncomment this line to request a gpu
+#SBATCH --cpus-per-task=4
+
+sleep $(( (RANDOM%10) + 1 )) # to avoid issues when submitting large amounts of jobs
+
+echo "SLURM_JOBID: " $SLURM_JOBID
+echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
+echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+
+echo "Job ID: ${SLURM_ARRAY_TASK_ID}"
+
+singularity exec --nv -B /scratch/$USER/NC_regression:/NC_regression -B /scratch/$USER/cql-sandbox/opt/conda/lib/python3.8/site-packages/mujoco_py/:/opt/conda/lib/python3.8/site-packages/mujoco_py/ /scratch/$USER/cql-sandbox bash -c "
+cd /NC_regression
+export PYTHONPATH=$PYTHONPATH:/NC_regression
+python main/exp/initiative/NC2.py --setting ${SLURM_ARRAY_TASK_ID}
+"
