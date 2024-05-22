ca8c18ffc6680396e74f3717fbcc386616d1dfd9
Konokiii <zd662@nyu.edu>
2024-05-22 13:59:07 +0800

    plot

---
+++ plots/log_alias.py
@@ -0,0 +1,27 @@
+reacher = 'DS500_Mno_relu_WD0.003'
+reacher_ds1000 = 'DS1000_Mno_relu_WD0.0015'
+new_reacher_wd15e3 = 'DS1000_Mnull_WD0.0015'
+new_reacher_wd5e5 = 'DS1000_Mnull_WD0.00005'
+new_reacher_wd0 = 'DS1000_Mnull_WD0'
+reacher_final = 'final'
+
+swimmer = 'DS1000_Mgelu_WD0.01'
+swimmer_ds1000 = 'DS1000_Mno_relu_WD0.025'
+swimmer_final = 'final'
+swimmer_wd0 = 'final_wd0'
+swimmer_wd5e5 = 'final_wd0.00005'
+swimmer_wd5e4 = 'final_wd0.0005'
+swimmer_wd5e3 = 'final_wd0.005'
+
+hopper_ds1000 = 'DS10000_Mno_relu_WD0.001'
+hopper_final = 'DS10000_Mno_relu_WD0.00095'
+
+carla2d = 'carla2d'
+carla2d_final = 'carla2d_new'
+carla2d_wd = 'carla2d_wd'
+
+carla1d = 'carla1d'
+carla1d_final = 'carla1d_new'
+
+age = 'age'
+age_final = 'age_new'
---
+++ plots/plot_learning_curves.py
@@ -0,0 +1,129 @@
+from quick_plot_helper import *
+from log_alias import *
+import matplotlib.pyplot as plt
+import matplotlib.image as pltimg
+
+# Global variables:
+default_performance_smooth = 5
+# font_size = {'title': 18,
+#              'legend': 12,
+#              'xlabel': 15,
+#              'ylabel': 15,
+#              'tick_label': 12}
+font_size = {'legend': 6}
+
+data_folder = '../results'
+save_folder = './figures/'
+
+y_use_log = None
+
+# dataset_group = [['reacher', 'swimmer', 'hopper'],
+#                  ['carla2d', 'carla1d', 'age']]
+# variant_group = [[reacher_final, swimmer_final, hopper_final],
+#                  [carla2d_final, carla1d_final, age_final]]
+# use_ratio = [((3/8, 1/40, 5/8), (0.25, 1/40, 0.25)), None]
+# dataset_group = [['reacher', 'swimmer', 'hopper', 'carla2d', 'carla1d', 'age']]
+# variant_group = [[reacher_final, swimmer_final, hopper_final, carla2d_final, carla1d_final, age_final]]
+# # use_ratio = [((3/8, 1/40, 5/8, 1, 1, 1), (0.25, 1/40, 0.25, 1, 1, 1))]
+# measure_group = [['NRC1', 'NRC2', 'NRC3_epoch'], ['trainError', 'testError']]
+# legend_group = [['NRC1', 'NRC2', 'NRC3'], ['Train MSE', 'Test MSE']]
+# xlabel = 'Epoch'
+# ylabel = ['NRC1-3', 'Accuracy']
+# # use_ratio = [None, None]
+# plot_full_datasets(dataset_group, variant_group, measure_group, legend_group,
+#                        figsize=None,
+#                        linewidth=1,
+#                        marker=None,
+#                        ms=6,
+#                        base_data_folder=data_folder,
+#                        save_name_prefix=f'figure2',
+#                        save_name_suffix=None,
+#                        save_folder_path=save_folder,
+#                        ymin=None,
+#                        ymax=None,
+#                        y_use_log=y_use_log,
+#                        y_log_scale=False,
+#                        xlabel=xlabel,
+#                        ylabel=ylabel,
+#                        font_size=font_size,
+#                        x_always_scientific=False,
+#                        use_ratio=[None, None]
+#                        )
+
+# dataset_group = [['reacher', 'swimmer', 'hopper', 'carla2d']]
+# variant_group = [[reacher_final, swimmer_final, hopper_final, carla2d_final]]
+# # use_ratio = [((3/8, 1/40, 5/8, 1, 1, 1), (0.25, 1/40, 0.25, 1, 1, 1))]
+# measure_group = [['NRC3_c']]
+# legend_group = [['NRC3']]
+# xlabel = '\u03B3'
+# ylabel = ['Last Epoch NRC3']
+# # use_ratio = [None, None]
+# plot_full_datasets(dataset_group, variant_group, measure_group, legend_group,
+#                        figsize=None,
+#                        linewidth=None,
+#                        marker=None,
+#                        ms=6,
+#                        base_data_folder=data_folder,
+#                        save_name_prefix=f'figure2',
+#                        save_name_suffix=None,
+#                        save_folder_path=save_folder,
+#                        ymin=None,
+#                        ymax=None,
+#                        y_use_log=y_use_log,
+#                        y_log_scale=False,
+#                        xlabel=xlabel,
+#                        ylabel=ylabel,
+#                        font_size=None,
+#                        x_always_scientific=False,
+#                        use_ratio=[None, None]
+#                        )
+
+
+datasets = ['swimmer', 'carla2d']
+variant_group = [[swimmer_wd0, swimmer_wd5e5, swimmer_wd5e4, swimmer_wd5e3], [carla2d_wd]]
+measures = ['NRC1', 'NRC2', 'NRC3_epoch']
+legend_group = [[r'$\lambda_{WD}=0$', r'$\lambda_{WD}=5e-4$', r'$\lambda_{WD}=5e-3$', r'$\lambda_{WD}=5e-2$', r'$\lambda_{WD}=1e-2$'],
+                [r'$\lambda_{WD}=0$', r'$\lambda_{WD}=5e-4$', r'$\lambda_{WD}=5e-3$', r'$\lambda_{WD}=5e-2$', r'$\lambda_{WD}=1e-2$']]
+xlabel = 'Epoch'
+ylabel = ['NRC1', 'NRC2', 'NRC3']
+plot_multi_variants(datasets, variant_group, measures, legend_group,
+                    figsize=None,
+                    linewidth=None,
+                    marker=None,
+                    ms=6,
+                    base_data_folder=data_folder,
+                    save_name_prefix=f'figure4',
+                    save_name_suffix=None,
+                    save_folder_path=save_folder,
+                    ymin=None,
+                    ymax=None,
+                    y_use_log=None,
+                    y_log_scale=False,
+                    xlabel=xlabel,
+                    ylabel=ylabel,
+                    font_size=None,
+                    x_always_scientific=False)
+
+# datasets = ['carla2d']
+# variant_group = [[carla2d_wd]]
+# measures = ['NRC1', 'NRC2', 'NRC3_epoch']
+# legend_group = [['WD=0', 'WD=5E-4', 'WD=5E-3', 'WD=5E-2', 'WD=1E-1']]
+# xlabel = 'Epoch'
+# ylabel = ['NRC1', 'NRC2', 'NRC3']
+# plot_multi_variants(datasets, variant_group, measures, legend_group,
+#                     figsize=None,
+#                     linewidth=None,
+#                     marker=None,
+#                     ms=6,
+#                     base_data_folder=data_folder,
+#                     save_name_prefix=f'figure4',
+#                     save_name_suffix=None,
+#                     save_folder_path=save_folder,
+#                     ymin=None,
+#                     ymax=None,
+#                     y_use_log=None,
+#                     y_log_scale=False,
+#                     xlabel=xlabel,
+#                     ylabel=ylabel,
+#                     font_size=font_size,
+#                     x_always_scientific=False)
