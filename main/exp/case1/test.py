f96c88eca67ec96a1b56058112f04486ccb3d15d
Zixuan Dong <zd662@login3.fast>
2024-06-26 12:45:10 +0400

    hpc conda

---
+++ main/exp/case1/test.py
@@ -0,0 +1,67 @@
+import os
+import torch
+import pyrallis
+import argparse
+
+from main.BC import TrainConfig, run_BC
+from main.utils import get_setting_dt
+
+CUDA_AVAILABLE = torch.cuda.is_available()
+if CUDA_AVAILABLE:
+    DEVICE = 'cuda'
+else:
+    DEVICE = 'cpu'
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--setting', type=int, default=0)
+    args = parser.parse_args()
+
+    settings = [
+        {'env': 'reacher',
+         'data_size': 1000,
+         'arch': '256-R-256-R-256-R|T',
+         'max_epochs': int(2),
+         'lamW': 1.5e-3},
+
+        {'env': 'swimmer',
+         'data_size': 1000,
+         'arch': '256-R-256-R-256-R|T',
+         'max_epochs': int(2),
+         'lamW': 1e-2},
+
+        {'env': 'hopper',
+         'data_size': 10000,
+         'arch': '256-R-256-R-256|T',
+         'max_epochs': int(2),
+         'lamW': 9.5e-4}
+    ]
+
+    actual_setting = settings[args.setting]
+
+    """replace values & global setup"""
+    config = TrainConfig(**actual_setting)
+    config.device = DEVICE
+    print(config.device)
+    config.lamH = -1
+    config.lr = 1e-2
+
+    config.eval_freq = 2
+
+    config.data_folder = './dataset/mujoco'
+    config.project = 'NC_explore'
+    config.group = 'test'
+    hyper2logname = {
+        'env': 'E',
+        'data_size': 'DS',
+        'lamW': 'wd',
+        'max_epochs': 'Eps'
+    }
+    config.name = '_'.join([v + str(getattr(config, k)) for k, v in hyper2logname.items() if v != ''])
+
+    run_BC(config)
+
+
+if __name__ == '__main__':
+    main()
---
+++ main/exp/case1/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --verbose
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=16GB
+#SBATCH --mail-type=ALL # select which email types will be sent
+#SBATCH --mail-user=zd662@nyu.edu # NOTE: put your netid here if you want emails
+
+#SBATCH --array=0-2 # here the number depends on number of tasks in the array, e.g. 0-11 will create 12 tasks
+#SBATCH --output=../logs/%A_%a.out # %A is SLURM_ARRAY_JOB_ID, %a is SLURM_ARRAY_TASK_ID,
+#SBATCH --error=../logs/%A_%a.err # MAKE SURE WHEN YOU RUN THIS, ../logs IS A VALID PATH
+
+# #####################################################
+# #SBATCH --gres=gpu:1 # uncomment this line to request a gpu
+#SBATCH --cpus-per-task=1
+
+sleep $(( (RANDOM%10) + 1 )) # to avoid issues when submitting large amounts of jobs
+
+echo "SLURM_JOBID: " $SLURM_JOBID
+echo "SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
+echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+
+echo "Job ID: ${SLURM_ARRAY_TASK_ID}"
+
+source /share/apps/NYUAD5/miniconda/3-4.11.0/bin/activate
+conda activate nrc
+export PYTHONPATH=$PYTHONPATH:$SCRATCH/NC_regression
+cd $SCRATCH/NC_regression
+python main/exp/case1/test.py --setting ${SLURM_ARRAY_TASK_ID}
