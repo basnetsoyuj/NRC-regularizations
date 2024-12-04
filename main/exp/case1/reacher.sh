1fb524ea43888b28ca3e7a85caf0557a24c92996
Soyuj Jung Basnet <bsoyuj@gmail.com>
2024-12-04 21:53:05 +0400

    sbatch time increase

--- main/exp/case1/reacher.sh
+++ main/exp/case1/reacher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --verbose
-#SBATCH --time=47:59:59
+#SBATCH --time=71:59:59
 #SBATCH --nodes=1
 #SBATCH --mem=16GB
 #SBATCH --mail-type=ALL # select which email types will be sent
